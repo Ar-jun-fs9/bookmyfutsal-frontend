d1d684b36d4faedd413d3f58c3b77af8f026ecb3
Ar-jun-fs9 <arjunbhul5799@gmail.com>
2026-01-13 13:21:16 +0545

    change date format in booking tacker playing date field

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -20,7 +20,7 @@ export const formatDate = (dateString: string): string => {
   const day = date.getDate().toString().padStart(2, '0');
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const year = date.getFullYear();
-  return `${day}-${month}-${year}`;
+  return `${year}-${month}-${day}`;
 };
 
 // Distance calculation
